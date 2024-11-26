61ffc4975343618f92249d4c44ba8cb12267ec2d
nejakasman <neja.kasman@student.um.si>
2024-11-26 17:05:31 +0100

    prijava in registracija - frontend

--- backend/src/main/java/com/example/recepti/UporabnikRepository.java
+++ backend/src/main/java/com/example/recepti/UporabnikRepository.java
@@ -1,9 +1,2 @@
-package com.example.recepti;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UporabnikRepository extends CrudRepository<Uporabnik, Integer> {
-    Uporabnik findByUporabniskoIme(String uporabniskoIme);
-    Uporabnik findByEmail(String email);
-
+package com.example.recepti;public interface UporabnikRepository {
 }
