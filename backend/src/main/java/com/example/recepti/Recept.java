49c67bc9cca3b01798b31ed1cdd801fc25226ef8
Anastasiya384 <veleicht@gmail.com>
2025-01-09 16:42:35 +0100

    Sprememba v razredo recept in KuharskiIzziv

--- backend/src/main/java/com/example/recepti/Recept.java
+++ backend/src/main/java/com/example/recepti/Recept.java
@@ -1,3 +1,75 @@
+//package com.example.recepti;
+//
+//import com.fasterxml.jackson.annotation.JsonBackReference;
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import jakarta.persistence.*;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import com.example.recepti.*;
+//
+//@Data
+//@NoArgsConstructor
+//@Entity
+//@Table(name = "recept")
+//@JsonIgnoreProperties("kuharskiIzziv") // Ignoriraj kuharskiIzziv pri serializaciji receptov
+//
+//public class Recept {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private int id;
+//
+//    private String ime;
+//    private String opis;
+//    private int casPriprave;
+//
+//
+//    @ElementCollection
+//    @CollectionTable(name = "navodila", joinColumns = @JoinColumn(name = "recept_id"))
+//    private List<String> navodila = new ArrayList<>();
+//
+//    @ElementCollection
+//    @CollectionTable(name = "sestavine", joinColumns = @JoinColumn(name = "recept_id"))
+//    private List<String> sestavine = new ArrayList<>();
+//
+//
+//    @Enumerated(EnumType.STRING)
+//    private Kategorija kategorija;
+//
+//    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recept")
+//    private List<Komentar> komentarji = new ArrayList<>();
+//
+//    @JsonBackReference // Prepreči serializacijo polja `kuharskiIzziv`
+//    @ManyToOne
+//    @JoinColumn(name = "kuharski_izziv_id")
+//    private KuharskiIzziv kuharskiIzziv;
+//
+//
+//    @ManyToOne
+//    @JoinColumn(name = "uporabnik_id")
+//    private Uporabnik uporabnik; // Povezava z uporabnikom
+//
+//    private double povprecnaOcena = 0.0;
+//    private int stOcen = 0;
+//
+//
+//    public Recept(String ime, List<String> navodila, List<String> sestavine, String opis, int casPriprave, Kategorija kategorija) {
+//        this.ime = ime;
+//        this.navodila = navodila;
+//        this.sestavine = sestavine;
+//        this.opis = opis;
+//        this.casPriprave = casPriprave;
+//        this.kategorija = kategorija;
+//    }
+//}
+//
+//
+//
+
+
 package com.example.recepti;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
@@ -8,14 +80,12 @@ import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
-import com.example.recepti.*;
 
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "recept")
 @JsonIgnoreProperties("kuharskiIzziv") // Ignoriraj kuharskiIzziv pri serializaciji receptov
-
 public class Recept {
 
     @Id
@@ -26,7 +96,6 @@ public class Recept {
     private String opis;
     private int casPriprave;
 
-
     @ElementCollection
     @CollectionTable(name = "navodila", joinColumns = @JoinColumn(name = "recept_id"))
     private List<String> navodila = new ArrayList<>();
@@ -34,7 +103,6 @@ public class Recept {
     @ElementCollection
     @CollectionTable(name = "sestavine", joinColumns = @JoinColumn(name = "recept_id"))
     private List<String> sestavine = new ArrayList<>();
-
 
     @Enumerated(EnumType.STRING)
     private Kategorija kategorija;
@@ -47,14 +115,12 @@ public class Recept {
     @JoinColumn(name = "kuharski_izziv_id")
     private KuharskiIzziv kuharskiIzziv;
 
-
     @ManyToOne
     @JoinColumn(name = "uporabnik_id")
     private Uporabnik uporabnik; // Povezava z uporabnikom
 
     private double povprecnaOcena = 0.0;
     private int stOcen = 0;
-
 
     public Recept(String ime, List<String> navodila, List<String> sestavine, String opis, int casPriprave, Kategorija kategorija) {
         this.ime = ime;
@@ -64,7 +130,13 @@ public class Recept {
         this.casPriprave = casPriprave;
         this.kategorija = kategorija;
     }
+
+    // Manjkajoči getterji
+    public double getPovprecnaOcena() {
+        return povprecnaOcena;
+    }
+
+    public int getStOcen() {
+        return stOcen;
+    }
 }
-
-
-
