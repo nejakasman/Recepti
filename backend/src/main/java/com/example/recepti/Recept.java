8d16a014461001e6f93cefc7642588a8e0dc321e
stasshev <130176653+stasshev@users.noreply.github.com>
2025-01-09 16:53:35 +0100

    ci-cd

--- backend/src/main/java/com/example/recepti/Recept.java
+++ backend/src/main/java/com/example/recepti/Recept.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
